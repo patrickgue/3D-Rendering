0c70def2d1d4ed178a854d33fce9cdc74f9bae09
patrickgue <me@patrickguenthard.ch>
2021-06-26 13:22:18 +0200

    model converter

--- util/model.c
+++ util/model.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "../vector.h"
 
@@ -13,12 +14,18 @@ int main(int argc, char **argv)
     int i, line_count = 0, polygons_count = 0, vec_buff_index = 0;
     poly *polygons = malloc(0);
     vec3 vec_buff[3];
+    bool debug_mode = false;
     
     
     if (argc < 3)
     {
 	usage();
 	exit(1);
+    }
+
+    if (argc == 4 && strcmp(argv[3], "-d") == 0)
+    {
+	debug_mode = true;
     }
 
     input_file = fopen(argv[1], "r");
@@ -46,15 +53,44 @@ int main(int argc, char **argv)
 	    line_count++;
 	    line_buffer[i-1] = 0;
 	    i = 0;
-	    printf("%2d: %s\n", line_count, line_buffer);
-	    vec_buff[vec_buff_inde++] = parse_vec3(line_buffer);
-	} 
+	    if (debug_mode)
+	    {
+		printf("%2d: %s\n", line_count, line_buffer);
+	    }
+		
+	    if (vec_buff_index < 3)
+	    {
+		vec_buff[vec_buff_index++] = parse_vec3(line_buffer);
+	    }
+	    else {
+		polygons = realloc(polygons, sizeof(poly) * (polygons_count + 1));
+		polygons[polygons_count].a = vec_buff[0];
+		polygons[polygons_count].b = vec_buff[1];
+		polygons[polygons_count].c = vec_buff[2];
+		polygons[polygons_count].color = strtol(line_buffer, NULL, 16);
+		polygons_count++;
+		vec_buff_index = 0;
+	    }
+	    
+	}
 
 
     }
-    
-    
-    
+
+    if (debug_mode)
+    {
+	for(i = 0; i < polygons_count; i++)
+	{
+	    printf("A: %2.1f / %2.1f / %2.1f\nB: %2.1f / %2.1f / %2.1f\nC: %2.1f / %2.1f / %2.1f\n%08x\n\n",
+		   polygons[i].a.x, polygons[i].a.y, polygons[i].a.z,
+		   polygons[i].b.x, polygons[i].b.y, polygons[i].b.z,
+		   polygons[i].c.x, polygons[i].c.y, polygons[i].c.z, polygons[i].color);
+	}
+    }
+
+
+    fwrite(&polygons_count, 1, sizeof(int), output_file);
+    fwrite(polygons, polygons_count, sizeof(poly), output_file);
 
     fclose(input_file);
     fclose(output_file);
